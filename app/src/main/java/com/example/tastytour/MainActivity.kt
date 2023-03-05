72aff14cf78d715ca27e759ac6b330d3844b0584
Parris <parris.small@warwick.ac.uk>
2023-03-05 14:22:26 +0000

    Firebase connected

--- app/src/main/java/com/example/tastytour/MainActivity.kt
+++ app/src/main/java/com/example/tastytour/MainActivity.kt
@@ -3,39 +3,33 @@ package com.example.tastytour
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.fragment.app.Fragment
 import com.example.tastytour.databinding.ActivityMainBinding
 
-
-
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding:ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        //loginpage
         binding.loginButton.setOnClickListener {
-            if (binding.emailLogin.text.isNullOrBlank() && binding.passwordLogin.text.isNullOrBlank()) {
-                Toast.makeText(this, "Please fill the required details", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "${binding.emailLogin.text} is logged in", Toast.LENGTH_SHORT)
-                    .show()
-            }
             // Move to homepage
-            val intent = Intent(this, HomeActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish() // Finish the current activity to prevent going back to login screen on back press
-        }
-
 
         }
 
+        //create account page
+        binding.CreateButton.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish() //
+
+        }
 
     }
-
+}
